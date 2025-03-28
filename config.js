b366a10e84d0122ed96f6534f226e24a06a10e0a
damiruimo <damirusanhitha05@gmail.com>
2025-03-28 22:27:48 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+
+module.exports = {
+
+SESSION_ID: process.env.SESSION_ID === undefined ? 'zRNhjCrR#5jjNsH2b2MVwI2X6p9alvzwhTGuAMbe1WUt9-0FOki4' : process.env.SESSION_ID, 
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'Fq8RyO35S1ndHiWJVHcDUjRDqyM61S20BO7C' : process.env.GITHUB_AUTH_TOKEN,  //🛑 Github token එක දැමීමේදී ghp_  කොටස ඉවර් කර දමන්න.  
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'imodamiru2' : process.env.GITHUB_USER_NAME, // 🛑 Github user name එක ගද්දි ඔයාලා token එක ගත්ත Github එකේ user name එක දාන්න අනිවාරෙන්
+PORT: process.env.PORT === undefined ? '8000' : process.env.PORT, 
+
+
+//🛑 Github token එක දැමීමේදී ghp_  කොටස ඉවර් කර දමන්න.
+//🛑 Github user name එක ගද්දි ඔයාලා token එක ගත්ත Github එකේ user name එක දාන්න අනිවාරෙන්
+
+};
